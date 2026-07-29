f6979b78cf0cb2dc30bb47ebb5606e5e971c18a5
thamilkumaranvg-org <thamilkumaran.2026@gmail.com>
2026-07-29 22:16:36 +0530

    Time: 5 ms (31.13%), Space: 43.9 MB (30.69%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,7 +1,6 @@
 class Solution {
     boolean containsSub(String str1,String str2){
         if(str2.length()>str1.length())return false;
-        
         for(int i=0;i<=str1.length()-str2.length();i++){
            int count=0;
             for(int j=0;j<str2.length();j++){
@@ -16,11 +15,9 @@ class Solution {
         List<String> lst=new ArrayList<>();
         for(String i:words){
             for(String j:words){
-                if(containsSub(i,j)&&i!=j){
-                    if(!lst.contains(j))lst.add(j);
-                }
+               if(i!=j&&containsSub(i,j))if(!lst.contains(j))lst.add(j);
             }
         }
-        return lst;
+         return lst;
     }
 }
