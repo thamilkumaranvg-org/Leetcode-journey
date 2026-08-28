7bbfa5612f8b57b18a57fd1923c62778ef1165fd
thamilkumaranvg-org <thamilkumaran.2026@gmail.com>
2026-08-28 23:04:39 +0530

    0014-LONGEST-COMMON-PREFIX Stats: Time: 1 ms (60.75%), Space: 43.1 MB (82.97%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,6 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length==1) return strs[0];
+      
         StringBuilder result = new StringBuilder();
         for(int i=0;i<strs[0].length();i++){
             for(String s : strs){
